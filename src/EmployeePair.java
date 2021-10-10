41a5988b17a0f8f91dc65c61c22b9b204dde4656
ddimov99 <81180152+ddimov99@users.noreply.github.com>
2021-10-11 01:33:54 +0300

    Update EmployeePair.java

--- src/EmployeePair.java
+++ src/EmployeePair.java
@@ -1,2 +1,16 @@
-public record EmployeePair(Employee e1,Employee e2) {
+import java.util.Objects;
+
+public record EmployeePair(Employee e1, Employee e2) {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof EmployeePair employeePair)) return false;
+        return this.e1.equals(((EmployeePair) obj).e1) && this.e2.equals(((EmployeePair) obj).e2)
+                || this.e1.equals(((EmployeePair) obj).e2)  && this.e2.equals(((EmployeePair) obj).e1);
+    }
+ 
+    @Override
+    public int hashCode() {
+        return Objects.hash(e1, e2);
+    }
 }
