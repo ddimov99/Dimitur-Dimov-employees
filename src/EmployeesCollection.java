208650bc04e9d8c56bd5f850e58f1e00f4ceba4f
ddimov99 <81180152+ddimov99@users.noreply.github.com>
2021-10-11 01:35:15 +0300

    Update EmployeesCollection.java

--- src/EmployeesCollection.java
+++ src/EmployeesCollection.java
@@ -6,9 +6,10 @@ import java.text.ParseException;
 import java.util.*;
 
 public class EmployeesCollection {
-
+ 
     List<Employee> employees;
-
+    Map<EmployeePair, Long> pairsWorkedTime = new HashMap<>();
+ 
     public EmployeesCollection(String fileName) {
         employees = new ArrayList<>();
         Path usersFilePath = Path.of(fileName);
@@ -21,7 +22,7 @@ public class EmployeesCollection {
                     int projectID = Integer.parseInt(parameters[1]);
                     String dateFrom = parameters[2];
                     String dateTo = parameters[3];
-
+ 
                     employees.add(new Employee(employeeID, projectID, dateFrom, dateTo));
                 }
             } catch (IOException e) {
@@ -32,9 +33,8 @@ public class EmployeesCollection {
         }
         employees.sort(Comparator.comparing(Employee::getProjectID));
     }
-
+ 
     public EmployeePair findLongestWorkingPair() {
-        long currLongestTime = 0;
         EmployeePair longestWorkingPair = null;
         Integer currProj = null;
         Integer nextProj = null;
@@ -44,20 +44,33 @@ public class EmployeesCollection {
             for (int j = i + 1;; ++j) {
                 if (j == employees.size()) {
                     break;
-                }
+                } // 63072000000
                 Employee nextEmpl = employees.get(j);
                 nextProj = nextEmpl.getProjectID();
                 if (nextProj == currProj) {
                     long timeWorkingTogether = currEmpl.getTimeWorkedWith(nextEmpl);
-                    if (timeWorkingTogether > currLongestTime) {
-                        longestWorkingPair = new EmployeePair(currEmpl, nextEmpl);
-                        currLongestTime = timeWorkingTogether;
+                    EmployeePair currPair = new EmployeePair(currEmpl, nextEmpl);
+                    if (!pairsWorkedTime.containsKey(currPair)) {
+                        pairsWorkedTime.put(currPair, timeWorkingTogether);
+                    } else {
+                        pairsWorkedTime.put(currPair, pairsWorkedTime.get(currPair) + timeWorkingTogether);
                     }
                 } else {
                     break;
                 }
             }
         }
-        return longestWorkingPair;
+        long maxTime = 0;
+        EmployeePair resultPair = null;
+ 
+        for (EmployeePair pair : pairsWorkedTime.keySet()) {
+            long currentPairTime = pairsWorkedTime.get(pair);
+            if (currentPairTime > maxTime) {
+                resultPair = pair;
+                maxTime = currentPairTime;
+            }
+        }
+ 
+        return resultPair;
     }
 }
