78064c9dc5b118fadf4c96029b6db9fa9dcb654c
ddimov99 <81180152+ddimov99@users.noreply.github.com>
2021-10-11 01:32:23 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -8,38 +8,36 @@ public class Employee {
     private final int projectID;
     private final Date dateFrom;
     private final Date dateTo;
-
+ 
     private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-
+ 
     public Employee(int empID, int projectID, String dateFrom, String dateTo) throws ParseException {
         this.empID = empID;
         this.projectID = projectID;
         this.dateFrom = formatter.parse(dateFrom);
         this.dateTo = dateTo.equals("NULL") ? new Date() : formatter.parse(dateTo);
     }
-
+ 
     public int getEmpID() {
         return empID;
     }
-
+ 
     public int getProjectID() {
         return projectID;
     }
-
+ 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Employee employee)) return false;
-        return empID == employee.empID && projectID == employee.projectID
-                && dateFrom.equals(employee.dateFrom)
-                && dateTo.equals(employee.dateTo);
+        return empID == employee.empID;
     }
-
+ 
     @Override
     public int hashCode() {
-        return Objects.hash(empID, projectID, dateFrom, dateTo);
+        return Objects.hash(empID);
     }
-
+ 
     @Override
     public String toString() {
         return "Employee{" +
@@ -49,13 +47,13 @@ public class Employee {
                 ", dateTo=" + dateTo +
                 '}';
     }
-
+ 
     public long getTimeWorkedWith(Employee other){
         final long thisFrom = this.dateFrom.getTime();
         final long thisTo = this.dateTo.getTime();
         final long otherFrom = other.dateFrom.getTime();
         final long otherTo = other.dateTo.getTime();
-
+ 
         if (thisFrom >= otherFrom && thisTo >= otherTo && otherTo > thisFrom) {
             return otherTo - thisFrom;
         } else if (thisFrom <= otherFrom && thisTo <= otherTo && thisTo > otherFrom) {
